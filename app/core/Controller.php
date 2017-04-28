070e54c3536de3ea5da2602c14da4cabe61ffa27
Tess ATTIE <tessattie@gmail.com>
2017-04-28 13:51:24 -0400

    Trying another connection type for memcache

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -13,7 +13,7 @@ class Controller{
 		$this->userRole = $this->setRole();
 
 		$this->memcache = new Memcache;
-		$this->memcache->addServer("localhost");
+		$this->memcache->pconnect("127.0.0.1", "11211");
 	}
 
 	public function model($model)
