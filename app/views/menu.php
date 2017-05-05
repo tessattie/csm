49b3028dd80870084f41be3e23b45e8ce0de93da
Tess ATTIE <tessattie@gmail.com>
2017-05-05 16:48:36 -0400

    Take out zero movement of level 2 menu

--- app/views/menu.php
+++ app/views/menu.php
@@ -13,31 +13,13 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown">
-          <a href="#" class="dropdown-toggle navrightmenu menuitems" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Zero Movement <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#" id="vendorMvt">Vendor</a>
-            <form method = 'POST' action = '/csm/public/home/vendorMovement' name='vendorMvtform' id = 'vendorMvtform'>
-                <input type='hidden' name = 'vendorMvtNumber' id = 'vendorMvtNumber'>
-                <input type='hidden' name = 'fromMvtvendor' id = 'fromMvtvendor'>
-                <input type='hidden' name = 'toMvtvendor' id = 'toMvtvendor'>
-              </form>
-            </li>
-            <li><a href="#" id="sectionMvt">Section</a>
-            <form method = 'POST' action = '/csm/public/home/sectionMovement' name='sectionMvtform' id = 'sectionMvtform'>
-              <input type='hidden' name = 'sectionMvtNumber' id = 'sectionMvtNumber'>
-              <input type='hidden' name = 'fromMvtsection' id = 'fromMvtsection'>
-              <input type='hidden' name = 'toMvtsection' id = 'toMvtsection'>
-            </form></li>
-            <li><a href="#" id="vendorSectionMvt">Vendor Section</a>
-            <form method = 'POST' action = '/csm/public/home/vendorSectionMovement' name='vendorSectionMvtform' id = 'vendorSectionMvtform'>
-              <input type='hidden' name = 'svendorMvtNumber' id = 'svendorMvtNumber'>
-              <input type='hidden' name = 'sctvendorMvtNumber' id = 'sctvendorMvtNumber'>
-              <input type='hidden' name = 'fromvendorMvtSection' id = 'fromvendorMvtSection'>
-              <input type='hidden' name = 'tovendorMvtSection' id = 'tovendorMvtSection'>
-            </form></li>
-          </ul>
-        </li>
+        <?php 
+        if($_SESSION['role'] != "3" && $_SESSION['role'] != "4")
+        {
+          include("zeroMovement.php");
+        } 
+        ?>
+      
         <li><a href="/csm/public/home/vendorNames" class="menuitems navrightmenu">Vendor list</a></li>
         <li><a href="/csm/public/home/sectionNames" class="menuitems navrightmenu">Section list</a></li>
         <li><a href="/csm/public/home/departmentNames" class="menuitems navrightmenu">Department list</a></li>
